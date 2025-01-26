ed9f507da32745980e23aec27e6b9dc6b9c8aec4
Harmeet10000 <harmeetsinghfbd@gmail.com>
2025-01-27 00:40:39 +0530

    Completed build scripts with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import path from "path";
+import webpackNodeExternals from "webpack-node-externals";
+import Dotenv from "dotenv-webpack";
+
+export default {
+  entry: "./src/index.js", // Entry point updated to src/index.js
+  target: "node",
+  mode: "production", // Use production mode for optimizations
+  externals: [webpackNodeExternals()], // Exclude node_modules from the bundle
+  output: {
+    filename: "index.js",
+    path: path.resolve(process.cwd(), "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"], // Transpile modern JavaScript
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+      path: "./.env.sample", // Use .env.sample file
+      systemvars: true, // Allow system variables to override
+    }),
+  ],
+  optimization: {
+    minimize: true, // Minify the output for production
+  },
+  stats: {
+    warningsFilter: /node_modules/, // Suppress warnings from node_modules
+  },
+};
